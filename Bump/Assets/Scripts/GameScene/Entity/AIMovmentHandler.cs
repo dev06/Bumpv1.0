e9b2786d1b60943f066bbdb38199a4292a88711f
dev06 <devanpatel13@gmail.com>
2016-07-28 21:18:05 -0400

    Updated AI movemnet handler

--- Bump/Assets/Scripts/GameScene/Entity/AIMovmentHandler.cs
+++ Bump/Assets/Scripts/GameScene/Entity/AIMovmentHandler.cs
@@ -5,7 +5,7 @@ public class AIMovmentHandler : EntityMovementHandler {
 
 	// Use this for initialization
 
-	public GameObject Ring; 
+	public GameObject Ring;
 
 	private Vector2 previousPosition;
 	private Vector2 force = Vector2.zero;
@@ -15,6 +15,17 @@ public class AIMovmentHandler : EntityMovementHandler {
 	private float updatePositionEvery = .5f;
 	private float frameCounter;
 
+	private float _straightDistanceOffset = 30;
+	private float _angle = 10;
+	private float angle;
+
+
+	//target instances
+	private MovementHandler targetMovementHandler;
+	private bool inContactWithTarget;
+
+
+
 	void Start () {
 		Init();
 		InitThis();
@@ -23,12 +34,17 @@ public class AIMovmentHandler : EntityMovementHandler {
 	void InitThis() {
 		target = GameObject.Find("Player");
 		previousPosition = target.transform.position;
+
+		if (target.GetComponent<MovementHandler>() != null)
+		{
+			targetMovementHandler = target.GetComponent<MovementHandler>();
+		}
 	}
 
 	// Update is called once per framesd
 	void Update () {
 		UseProjectedTrajectory();
-		CanBoost(); 
+		UseEvasion();
 
 	}
 
@@ -42,14 +58,11 @@ public class AIMovmentHandler : EntityMovementHandler {
 
 	private void AddSimpleForce(Vector2 targetVec)
 	{
+
+
 		Vector2 direction = targetVec - new Vector2(transform.position.x, transform.position.y);
 		FaceFoward(transform, direction);
-		// bool boosting; 
-		// Boost(direction, 10f, out boosting); 
-		// if(boosting){
-		// 	AddExternalObject(Ring, transform.position, transform.rotation); 
-		// 	boosting = false; 
-		// }
+
 		if (Vector2.Distance(targetVec, transform.position) > minForceDistance) {
 			rg2d.AddForce(direction.normalized);
 		}
@@ -78,5 +91,39 @@ public class AIMovmentHandler : EntityMovementHandler {
 		AddSimpleForce(force);
 	}
 
+	void OnCollisionStay2D(Collision2D col)
+	{
+		inContactWithTarget = true;
+	}
+
+	void OnCollisionExit2D(Collision2D col)
+	{
+		inContactWithTarget = false;
+	}
+
+
+	private void UseEvasion() {
+		bool withinRange = Vector2.Distance(target.transform.position, transform.position) < .4f;
+
+		if (withinRange && Random.Range(0, 20) < 4)
+		{
+			Vector2 opposingForce = transform.position - target.transform.position;
+			float angle = (int)GetAngle();
+			Vector2 addedForce = new Vector2(Mathf.Sin(angle) * Random.Range(-1, 1), Mathf.Cos(angle) * Random.Range(-1, 1));
+
+			Vector2 finalForce = (opposingForce * 15.0f) + addedForce * 15.0f;
+			FaceFoward(transform, finalForce);
+			rg2d.AddForce(finalForce);
+		}
+	}
+
+	private float GetAngle()
+	{
+		float dy = transform.position.y - target.transform.position.y;
+		float dx = transform.position.x - target.transform.position.x;
+
+		angle = Mathf.Atan2(dy, dx) * Mathf.Rad2Deg;
+		return angle;
+	}
 
 }
